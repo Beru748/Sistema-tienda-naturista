ced1cba788428c9e93a478eba5f5d089b9fff0ff
Beru748 <ericksamirwaton@gmail.com>
2026-04-07 08:32:30 -0500

    chore: actualizar gitignore para soportar archivos no deseados

---
+++ tiendanaturista/src/main/java/com/example/controller/SecondaryController.java
@@ -0,0 +1,13 @@
+package com.example.controller;
+import com.example.App;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class SecondaryController {
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("primary");
+    }
+}
