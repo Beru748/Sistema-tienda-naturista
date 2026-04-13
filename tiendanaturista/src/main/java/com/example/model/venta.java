69a70a4f3ca1f1143e149ed6668d33ffce9c25fb
Camilo Gonzalez <cagonzalezgarcia@unicesar.edu.co>
2026-04-12 21:14:49 -0500

    feat:implementacion getters y setters - clase venta

--- tiendanaturista/src/main/java/com/example/model/venta.java
+++ tiendanaturista/src/main/java/com/example/model/venta.java
@@ -15,6 +15,26 @@ public class venta {
         this.total=total;
         this.idCliente=idCliente;
     }
+    public int getIdVenta(){
+    return idVenta;
+    }
+    public void setIdVenta(int idVenta){
+      this.idVenta=idVenta;
+    }
+
+    public LocalDateTime getFechaHora(){
+        return fechaHora;
+    }
+    public void setFechaHora(LocalDateTime fechaHora){
+       this.fechaHora=fechaHora;
+    }
+
+    public double getTotal(){
+        return total;
+    }
+    public void setTotal(double total){
+        this.total=total;
+    }
     
 
 
