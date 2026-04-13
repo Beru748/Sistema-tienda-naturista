c459f5c6871ccc479f6a28b83c63ff85a7883b31
Camilo Gonzalez <cagonzalezgarcia@unicesar.edu.co>
2026-04-12 22:37:45 -0500

    feat:implementacion getters y setters - clase detalleVenta

--- tiendanaturista/src/main/java/com/example/model/detalleVEnta.java
+++ tiendanaturista/src/main/java/com/example/model/detalleVEnta.java
@@ -16,7 +16,47 @@ public class detalleVenta {
         this.precioUnitario=precioUnitario;
         this.subTotal=subTotal;
     }
+    public int getIdDetalle(){
+        return idDetalle;
+    }
+    public void setIdDetalle(int idDe   ){
+        this.idDetalle=idDe;
+    }
 
+    public int getIdVenta(){
+        return idVenta;
+    }
+    public void setIdVenta(int idVenta){
+        this.idVenta=idVenta;
+    }
+
+    public int getIdProducto(){
+        return idProducto;
+    }
+    public void setIdProducto(int idProducto){
+        this.idProducto=idProducto;
+    }
+
+    public int getCantidad(){
+        return cantidad;
+    }
+    public void setCantidad(int cantidad){
+        this.cantidad=cantidad;
+    }
+
+    public double getPrecioUnitario(){
+        return precioUnitario;
+    }
+    public void setPrecioUnitario(double precioUnitario){
+        this.precioUnitario=precioUnitario;
+    }
+
+    public double getSubTotal(){
+        return subTotal;
+    }
+    public void setSubTotal(double subTotal){
+        this.subTotal=subTotal;
+    }
     
 
 }
