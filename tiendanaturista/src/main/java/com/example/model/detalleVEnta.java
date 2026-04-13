408a032f33af0d51d6bc8ce1defcf34e72fecac2
Camilo Gonzalez <cagonzalezgarcia@unicesar.edu.co>
2026-04-12 22:36:55 -0500

    feat:implementacion constructor - clase detalleVenta

--- tiendanaturista/src/main/java/com/example/model/detalleVEnta.java
+++ tiendanaturista/src/main/java/com/example/model/detalleVEnta.java
@@ -8,4 +8,15 @@ public class detalleVenta {
     double precioUnitario;
     double subTotal;
 
+    public detalleVenta(int idDetalle, int idVenta, int idProducto, int cantidad,double precioUnitario,double subTotal){
+        this.idDetalle=idDetalle;
+        this.idVenta=idVenta;
+        this.idProducto=idProducto;
+        this.cantidad=cantidad;
+        this.precioUnitario=precioUnitario;
+        this.subTotal=subTotal;
+    }
+
+    
+
 }
