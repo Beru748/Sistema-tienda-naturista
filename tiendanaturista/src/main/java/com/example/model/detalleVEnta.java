08566777e298cec97c8e6bf74a4d0da494bbc784
Camilo Gonzalez <cagonzalezgarcia@unicesar.edu.co>
2026-04-12 22:29:38 -0500

    feat:implementacion atributos - clase detalleVenta

--- tiendanaturista/src/main/java/com/example/model/detalleVEnta.java
+++ tiendanaturista/src/main/java/com/example/model/detalleVEnta.java
@@ -1,5 +1,11 @@
 package com.example.model;
 
 public class detalleVenta {
+    int idDetalle;
+    int idVenta;
+    int idProducto;
+    int cantidad;
+    double precioUnitario;
+    double subTotal;
 
 }
