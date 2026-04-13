e2fa061bbe2f231d66b461ef50e481c8149f625e
Camilo Gonzalez <cagonzalezgarcia@unicesar.edu.co>
2026-04-12 21:54:23 -0500

    feat: implementacion getters y setters - clase categoria

--- tiendanaturista/src/main/java/com/example/model/categoria.java
+++ tiendanaturista/src/main/java/com/example/model/categoria.java
@@ -11,4 +11,28 @@ public class categoria {
         this.descripcion=descripcion;
     }
 
+    public int getIdCategoria(){
+        return idCategoria;
+    }
+
+    public void setIdCategoria(int idCategoria){
+        this.idCategoria=idCategoria;
+    }
+    
+    public String getNombre(){
+        return nombre;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre=nombre;
+    }
+
+    public String getDescripcion(){
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion){
+        this.descripcion=descripcion;
+    }
+
 }
