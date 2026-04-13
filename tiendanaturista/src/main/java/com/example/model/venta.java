02a6a2a17b21892ce32b1dacb845b550c6137aa8
Camilo Gonzalez <cagonzalezgarcia@unicesar.edu.co>
2026-04-12 21:11:08 -0500

    feat: implementacion de atributos - clase venta

--- tiendanaturista/src/main/java/com/example/model/venta.java
+++ tiendanaturista/src/main/java/com/example/model/venta.java
@@ -1,5 +1,14 @@
 package com.example.model;
 
+import java.time.LocalDateTime;
+
 public class venta {
+    int idVenta;
+    LocalDateTime fechaHora;
+    double total;
+    String metodoPago;
+    int idCliente;
+    
+
 
 }
