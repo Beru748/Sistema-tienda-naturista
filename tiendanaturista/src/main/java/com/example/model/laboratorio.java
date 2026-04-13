ad045de93bbdcdd9ede134af23a65f6895beb904
Camilo Gonzalez <cagonzalezgarcia@unicesar.edu.co>
2026-04-12 22:47:38 -0500

    feat: implementacion getters y setters - clase laboratorio

--- tiendanaturista/src/main/java/com/example/model/laboratorio.java
+++ tiendanaturista/src/main/java/com/example/model/laboratorio.java
@@ -10,4 +10,28 @@ public class laboratorio {
         this.nombre=nombre;
         this.telefono=telefono;
     }
+
+    public int getIdLaboratotio(){
+        return idLaboratorio;
+    }
+
+    public void setIdLaboratoio(int idLaboratoio){
+        this.idLaboratorio=idLaboratoio;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre=nombre;
+    }
+
+    public String getTelefono(){
+        return telefono;
+    }
+
+    public void setTelefono(String telefono){
+        this.telefono=telefono;
+    }
 }
