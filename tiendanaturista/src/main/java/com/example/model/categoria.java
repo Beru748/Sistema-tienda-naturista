7c9c8d8a13d94cdce3fce4898457dc7fbb32ef78
Camilo Gonzalez <cagonzalezgarcia@unicesar.edu.co>
2026-04-12 21:53:11 -0500

    feat: implementacion constructor - clase categoria

--- tiendanaturista/src/main/java/com/example/model/categoria.java
+++ tiendanaturista/src/main/java/com/example/model/categoria.java
@@ -1,7 +1,14 @@
 package com.example.model;
 
 public class categoria {
-       private int idCategoria;
-        private String nombre;
-        private String descripcion;
+    private int idCategoria;
+    private String nombre;
+    private String descripcion;
+
+    public categoria(int idCategoria, String nombre, String descripcion){
+        this.idCategoria= idCategoria;
+        this.nombre=nombre;
+        this.descripcion=descripcion;
+    }
+
 }
