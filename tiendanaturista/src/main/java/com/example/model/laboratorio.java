8a6a9d88e7b7a3dfba590062751a33dbad10c315
Camilo Gonzalez <cagonzalezgarcia@unicesar.edu.co>
2026-04-12 22:46:51 -0500

    feat: implementacion constructor - clase laboratorio

--- tiendanaturista/src/main/java/com/example/model/laboratorio.java
+++ tiendanaturista/src/main/java/com/example/model/laboratorio.java
@@ -4,4 +4,10 @@ public class laboratorio {
     int idLaboratorio;
     String nombre;
     String telefono;
+
+    public laboratorio(int idLaboratorio, String nombre, String  telefono){
+        this.idLaboratorio=idLaboratorio;
+        this.nombre=nombre;
+        this.telefono=telefono;
+    }
 }
