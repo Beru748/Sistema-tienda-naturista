b4da550bc2049b1e03aa890ee7548c7b292a8286
Camilo Gonzalez <cagonzalezgarcia@unicesar.edu.co>
2026-04-12 21:12:30 -0500

    feat: implementacion de constructor - clase venta

--- tiendanaturista/src/main/java/com/example/model/venta.java
+++ tiendanaturista/src/main/java/com/example/model/venta.java
@@ -8,6 +8,13 @@ public class venta {
     double total;
     String metodoPago;
     int idCliente;
+
+    public venta(int idVenta, LocalDateTime fechaHora, double total, String metodoPago, int idCliente){
+        this.idVenta=idVenta;
+        this.fechaHora=fechaHora;
+        this.total=total;
+        this.idCliente=idCliente;
+    }
     
 
 
