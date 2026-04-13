ce4592053e1c6590e613b34282ca83bd9ec84a00
Camilo Gonzalez <cagonzalezgarcia@unicesar.edu.co>
2026-04-12 22:50:12 -0500

    feat: implementacion atributos - clase producto

--- tiendanaturista/src/main/java/com/example/model/producto.java
+++ tiendanaturista/src/main/java/com/example/model/producto.java
@@ -1,5 +1,18 @@
 package com.example.model;
 
+import java.time.LocalDate;
+
 public class producto {
+    int idProducto;
+    String codigoBarras;
+    String nombre;
+    String descripcion;
+    double precio;
+    int stock;
+    String unidadMedida;
+    LocalDate fechaVencimiento;
+    boolean activo;
+    int idCategoria;
+    int idLaboratorio;
 
 }
