fa19bb7724872ce42ec7098e7cda93c47d4f6665
Beru748 <ericksamirwaton@gmail.com>
2026-04-26 16:40:43 -0500

    feat: creación de interfaz de gestión de ventas

---
+++ tiendanaturista/src/main/java/com/example/controller/DashboardController.java
@@ -0,0 +1,14 @@
+package com.example.controller;
+import com.example.App;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class DashboardController {
+
+}
