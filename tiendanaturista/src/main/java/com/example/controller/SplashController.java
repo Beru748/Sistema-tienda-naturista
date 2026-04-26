af889da6f03168b7b360d38ac092dc3ce3d43954
Beru748 <ericksamirwaton@gmail.com>
2026-04-25 21:59:09 -0500

    refactor: mejora en nomenclatura de archivos

--- tiendanaturista/src/main/java/com/example/controller/SplashController.java
+++ tiendanaturista/src/main/java/com/example/controller/SplashController.java
@@ -4,10 +4,10 @@ import com.example.App;
 import java.io.IOException;
 import javafx.fxml.FXML;
 
-public class PrimaryController {
+public class SplashController {
 
     @FXML
     private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+        App.setRoot("primary");
     }
 }
