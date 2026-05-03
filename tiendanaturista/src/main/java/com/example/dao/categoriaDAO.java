65dea66ab646665d09ed8518b95fe13dcbafd263
Camilo Gonzalez <cagonzalezgarcia@unicesar.edu.co>
2026-05-02 21:54:06 -0500

    feat:implementacion metodo insertar en categoriaDAO

--- tiendanaturista/src/main/java/com/example/dao/categoriaDAO.java
+++ tiendanaturista/src/main/java/com/example/dao/categoriaDAO.java
@@ -8,6 +8,24 @@ import com.example.util.ConexionDB;
 
 public class categoriaDAO {
 
-    
+    //conexion privada a la base de datos
+    private final Connection con = ConexionDB.obtenerConexion();
+
+    //insertar una nueva categoria en la base de datos
+    public boolean insertar(categoria cat){
+
+        String sql = "INSERT INTO categoria VALUES (seq_categoria.NEXTVAL, ?, ?)";
+
+        try (PreparedStatement ps = con.prepareStatement(sql)){
+
+            ps.setString(1, cat.getNombre());
+            ps.setString(2, cat.getDescripcion());
+            return ps.executeUpdate() > 0;
+
+        }catch(SQLException e){
+            System.err.println("Error al insertar categoria: " + e.getMessage());
+            return false;
+        }
+    }
 
 }
