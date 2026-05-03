2afee9e6e54f575cc68c67f49838fb462fd26242
Camilo Gonzalez <cagonzalezgarcia@unicesar.edu.co>
2026-05-02 22:06:26 -0500

    feat: implementacion metodos listarTodos  y mapear en categoriaDAO

--- tiendanaturista/src/main/java/com/example/dao/categoriaDAO.java
+++ tiendanaturista/src/main/java/com/example/dao/categoriaDAO.java
@@ -1,7 +1,11 @@
 package com.example.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.example.model.categoria;
 import com.example.util.ConexionDB;
@@ -23,9 +27,35 @@ public class categoriaDAO {
             return ps.executeUpdate() > 0;
 
         }catch(SQLException e){
-            System.err.println("Error al insertar categoria: " + e.getMessage());
+            System.err.println("Error al insertar la categoria: " + e.getMessage());
             return false;
         }
+    }
+
+    //lista todas laas categorias registradas
+    public List<categoria> listarTodos(){
+        List<categoria> lista = new ArrayList<>();
+        String sql = "SELECT * FROM categoria";
+        
+        try(Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery(sql)){
+                while(rs.next()){
+                    lista.add(mapear(rs));
+                }
+            }catch(SQLException e){
+                System.err.println("Error al listar las categorias: " + e.getMessage());
+            }
+            return lista;
+
+    }
+
+    private categoria mapear(ResultSet rs) throws SQLException{
+        
+        int idCategoria = rs.getInt("id_categoria");
+        String nombre = rs.getString("nombre");
+        String descripcion = rs.getString("descripcion");
+
+        return new categoria(idCategoria, nombre, descripcion);
     }
 
 }
